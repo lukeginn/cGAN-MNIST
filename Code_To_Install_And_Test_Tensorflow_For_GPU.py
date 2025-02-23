56d3ac2a2b464f784b0792bae0ec7422c30972b3
Luke Ginn <luke.ginn@hotmail.com>
2025-02-23 10:16:39 +1000

    cleaning up old code

--- Code_To_Install_And_Test_Tensorflow_For_GPU.py
+++ Code_To_Install_And_Test_Tensorflow_For_GPU.py
@@ -1,18 +1,18 @@
+# Use the following code to get your computer get for GPU deep learning
+# conda update --force conda
+# conda create --name gpu_env keras tensorflow cudatoolkit cudnn
+# conda activate gpu_env
+# pip install tensorflow-addons
 
-#Use the following code to get your computer get for GPU deep learning
-#conda update --force conda
-#conda create --name gpu_env tensorflow-gpu cudatoolkit cudnn keras
-#conda activate gpu_env
-#pip install tensorflow-addons
+# Test 1
+print("hi")
 
-#Test 1
-print('hi')
-
-#Test 2
+# Test 2
 import tensorflow as tf
-Image_Input = tf.keras.layers.Input(shape=(28,28,1))
 
-#Test 3
+Image_Input = tf.keras.layers.Input(shape=(28, 28, 1))
+
+# Test 3
 import tensorflow as tf
 from tensorflow.python.client import device_lib
 from tensorflow.python.keras import backend
@@ -21,6 +21,6 @@ from tensorflow.compat.v1 import ConfigProto, Session
 print(device_lib.list_local_devices())
 print("Num of GPUs available: ", len(tf.test.gpu_device_name()))
 if tf.test.gpu_device_name():
-    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+    print("Default GPU Device: {}".format(tf.test.gpu_device_name()))
 else:
     print("Please install GPU version of TF")
